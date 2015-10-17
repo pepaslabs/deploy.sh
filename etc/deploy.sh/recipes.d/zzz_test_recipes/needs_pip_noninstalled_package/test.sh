2500fb0a7da682a8e5716a402104dae0903578c0
Jason Pepas <jasonpepas@gmail.com>
2015-10-17 01:00:42 -0500

    updating needs_pip.sh and adding some test recipes

---
+++ etc/deploy.sh/recipes.d/zzz_test_recipes/needs_pip_noninstalled_package/test.sh
@@ -0,0 +1,4 @@
+echo_step "${color_yellow}needs_pip_noninstalled_package/test.sh${color_off} is running."
+
+# install a package which is unlikely to exist on the user's system.
+needs.sh pip oroboros
--- lib/deploy.sh/needs_pip.sh
+++ lib/deploy.sh/needs_pip.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-set -eu -o pipefail
+# needs_pip.sh: subcommand of needs.sh
+# see https://github.com/pepaslabs/deploy.sh
+
+
+# "strict" mode
+# thanks to http://stackoverflow.com/a/13099228
+# thanks to https://sipb.mit.edu/doc/safe-shell/
+set -o pipefail    # trace ERR through pipes
+set -o errtrace    # trace ERR through 'time command' and other functions
+set -o nounset     # set -u : exit the script if you try to use an uninitialised variable
+set -o errexit     # set -e : exit the script if any statement returns a non-true return value
+shopt -s failglob  # if a glob doesn't expand, fail.
+
+
+source "${deploysh_lib_dir}/exit_codes.bash"
+
+echo_step_component=needs/pip
 
 needs.sh apt python-pip
 
@@ -8,6 +24,15 @@ for pkg in "${@}"
 do
     if ! pip_pkg_is_installed.sh "${pkg}"
     then
-        pip install "${pkg}"
+        echo_step "Installing ${color_yellow}${pkg}${color_off}"
+        pip install "${pkg}" || \
+        (
+        	exit_status=$?
+    		echo_step_error "${color_yellow}pip install ${pkg}${color_off} exited status $exit_status."
+    		exit $exit_status
+        )
+        echo_step_ok "${color_yellow}${pkg}%{color_off} installed."
+    else
+        echo_step "Skipping ${color_yellow}${pkg}${color_off} (already installed)."
     fi
 done
