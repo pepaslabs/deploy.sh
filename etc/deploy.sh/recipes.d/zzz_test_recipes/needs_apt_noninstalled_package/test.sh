75b1415bdee0f719098c1c69e86d0a005d491c70
Jason Pepas <jasonpepas@gmail.com>
2015-10-17 01:23:59 -0500

    removing packages at end of tests to ensure the tests are repeatable

--- etc/deploy.sh/recipes.d/zzz_test_recipes/needs_apt_noninstalled_package/test.sh
+++ etc/deploy.sh/recipes.d/zzz_test_recipes/needs_apt_noninstalled_package/test.sh
@@ -3,3 +3,7 @@ echo_step "${color_yellow}needs_apt_noninstalled_package/test.sh${color_off} is 
 # install a package which is unlikely to exist on the user's system.
 # likelyhood determined by http://popcon.debian.org/
 needs.sh apt votca-csg-tutorials
+
+# remove it so the test is still useful next time
+echo_step "Removing ${color_yellow}votca-csg-tutorials${color_off}."
+apt-get remove votca-csg-tutorials
--- etc/deploy.sh/recipes.d/zzz_test_recipes/needs_pip_noninstalled_package/test.sh
+++ etc/deploy.sh/recipes.d/zzz_test_recipes/needs_pip_noninstalled_package/test.sh
@@ -2,3 +2,7 @@ echo_step "${color_yellow}needs_pip_noninstalled_package/test.sh${color_off} is 
 
 # install a package which is unlikely to exist on the user's system.
 needs.sh pip oroboros
+
+# remove it so the test is still useful next time
+echo_step "Removing ${color_yellow}oroboros${color_off}."
+pip uninstall oroboros
