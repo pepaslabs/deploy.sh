360c327e0ff3cfe41effadb1f442fc6843461605
Jason Pepas <jasonpepas@gmail.com>
2015-10-18 02:02:56 -0500

    adding colorization to test_is_nslu2

--- etc/deploy.sh/recipes.d/zzz_test_recipes/conditionals.bash/test_is_nslu2.sh
+++ etc/deploy.sh/recipes.d/zzz_test_recipes/conditionals.bash/test_is_nslu2.sh
@@ -1,6 +1,6 @@
 if is_nslu2
 then
-    echo_step "is NSLU2."
+    echo_step_color "${color_green}" "is NSLU2."
 else
-    echo_step "is not NSLU2."
+    echo_step_color "${color_red}" "is not NSLU2."
 fi
